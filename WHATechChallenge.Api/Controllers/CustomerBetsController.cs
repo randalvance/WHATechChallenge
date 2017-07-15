d948f2c564cb8904d18a01238bf223207a8279d9
Randal Cunanan <randal@randalvance.net>
2017-07-15 21:27:30 +0800

    Adding CustomerBetsController

---
+++ WHATechChallenge.Api/Controllers/CustomerBetsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using WHATechChallenge.Api.Models;
+using WHATechChallenge.Api.Services;
+
+namespace WHATechChallenge.Api.Controllers
+{
+	[Route("api/[controller]")]
+	public class CustomerBetsController : Controller
+	{
+		private ICustomerBetsCalculator customerBetsCalculator;
+
+		public CustomerBetsController(ICustomerBetsCalculator customerBetsCalculator)
+		{
+			this.customerBetsCalculator = customerBetsCalculator;
+		}
+
+		[HttpGet]
+		public async Task<List<CustomerBet>> Get()
+		{
+			return await this.customerBetsCalculator.CalculateCustomerBetsAsync();
+		}
+	}
+}
